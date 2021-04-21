33f6214e7d0d810c7c1a45bff81f0bc24f30b201
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2021-04-20 22:26:47 -0300

    criado model de anuncio

--- projeto6/anuncio/admin.py
+++ projeto6/anuncio/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from anuncio import models
 
-# Register your models here.
+admin.site.register(models.Categoria)
+admin.site.register(models.Anuncio)
--- projeto6/anuncio/models.py
+++ projeto6/anuncio/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+  titulo = models.CharField(max_length=60)
+
+  def __str__(self):
+    return self.titulo
+
+
+class Anuncio(models.Model):
+  titulo = models.CharField(max_length=60)
+  descricao = models.TextField(null=True, blank=True)
+  preco = models.DecimalField(max_digits=11, decimal_places=2)
+  categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+  criado_em = models.DateTimeField(auto_now_add=True)
+  alterado_em = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.titulo
